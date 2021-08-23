6cfe8eaab9e6974c0633879ae85e59da182d5006
mariiakny <70655664+mariiakny@users.noreply.github.com>
2021-08-23 09:27:16 +0300

    Create ввод_вывод.py

---
+++ ввод_вывод.py
@@ -0,0 +1,27 @@
+# В корзине лежит 29 яблок, Вика отдала друзьям 17 яблока.
+#all = 30
+#b = 17
+#print(all - b)
+a = 4567
+b = "ЛЕто!"
+c = 78.9
+# int(целое число), float (число с плавающей точкой), str (строка)
+#print(type(b))
+
+# Функция ввода 
+#all_apples = int(input("Введите количество яблок"))    # Это важно!!!!!!
+#print(type(all_apples))
+n = 45
+#print(float(n))
+#print(type(str(n)))
+
+# Функция вывода
+print(a, b, c, n)
+print(c, '+', n, '=', c+n)
+a = 4
+b = 8
+print(a, '+', b, '=', a+b, sep='')
+a = 4
+b = 8
+print(a, '+', b, '=', a+b, end=' ')
+print(a)
